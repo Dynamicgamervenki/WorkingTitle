6c0f80f43a07692445efd576edc670e0ab7e08eb
SuryaKumar135 <122425952+SuryaKumar135@users.noreply.github.com>
2024-08-04 17:54:20 +0530

    basic jump implement still has to compete

--- WorkingTitle/Assets/rootmotionController/scripts/RootMotionController.cs
+++ WorkingTitle/Assets/rootmotionController/scripts/RootMotionController.cs
@@ -1,7 +1,5 @@
-using Cinemachine;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 [RequireComponent(typeof(CharacterController))]
 public class RootMotionController : MonoBehaviour
 {
@@ -18,6 +16,24 @@ public class RootMotionController : MonoBehaviour
     float _runValue;
     [HideInInspector] public bool animationBusy;
     Animator _animator;
+    CharacterController _characterController;
+
+    //Jump Values
+    [Header("Player Grounded")]
+    [Tooltip("If the character is grounded or not. Not part of the CharacterController built in grounded check")]
+    public bool Grounded = true;
+
+    [Tooltip("Useful for rough ground")]
+    public float GroundedOffset = -0.14f;
+
+    [Tooltip("The radius of the grounded check. Should match the radius of the CharacterController")]
+    public float GroundedRadius = 0.28f;
+
+    [Tooltip("What layers the character uses as ground")]
+    public LayerMask GroundLayers;
+    bool jumpPerformed;
+    [SerializeField] float JumpForce;
+
 
 
     //Cine machine
@@ -26,13 +42,16 @@ public class RootMotionController : MonoBehaviour
     private void Start()
     {
         _animator = GetComponent<Animator>();
+        _characterController=GetComponent<CharacterController>();
         //followCam.Priority = 15;
     }
     private void Update()
     {
         movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        jumpPerformed=Input.GetKeyDown(KeyCode.Space);
         moveAmount = Mathf.Clamp01(Mathf.Abs(movement.x) + Mathf.Abs(movement.z));
         Locomotion();
+        GroundedCheck();
         //_animator.SetFloat("moveX", movement.x, damValue, Time.deltaTime);
         //_animator.SetFloat("moveY", movement.z, damValue, Time.deltaTime);
 
@@ -44,9 +63,10 @@ public class RootMotionController : MonoBehaviour
     }
     void Locomotion()
     {
+
         _runValue = (Input.GetKey(KeyCode.LeftShift)) ? 5f : 1f;
         _animator.SetFloat("Locomotion", moveAmount * _runValue, damValue, Time.deltaTime);
-
+        if(jumpPerformed) { _animator.SetTrigger("Jump"); _characterController.Move(Vector3.up* JumpForce); }
         movement = (Quaternion.LookRotation(new Vector3(cam.x, 0f, cam.z)) * movement);
         if (animationBusy) { return; }
         if (moveAmount > 0f)
@@ -71,6 +91,25 @@ public class RootMotionController : MonoBehaviour
             transform.rotation = Quaternion.RotateTowards(transform.rotation, target, degreeDelta * Time.deltaTime);
         }
     }
+    private void GroundedCheck()
+    {
+        // set sphere position, with offset
+        Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - GroundedOffset,
+            transform.position.z);
+        Grounded = Physics.CheckSphere(spherePosition, GroundedRadius, GroundLayers,
+            QueryTriggerInteraction.Ignore);
+
+        // update animator if using character
+        if (_animator)
+        {
+            _animator.SetBool("IsGrounded", Grounded);
+        }
+    }
+
+    private void JumpAndGravity()
+    {
+       
+    }
 
     public void PlayerBalance()
     {
@@ -79,5 +118,20 @@ public class RootMotionController : MonoBehaviour
     public void PlayerBalanceComplete()
     {
         _animator.SetLayerWeight(2, 0f);
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Color transparentGreen = new Color(0.0f, 1.0f, 0.0f, 0.35f);
+        Color transparentRed = new Color(1.0f, 0.0f, 0.0f, 0.35f);
+
+        if (Grounded) Gizmos.color = transparentGreen;
+        else Gizmos.color = transparentRed;
+
+        // when selected, draw a gizmo in the position of, and matching radius of, the grounded collider
+        Gizmos.DrawSphere(
+            new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z),
+            GroundedRadius);
     }
 }
