da4772d558e0eac4581a9a1ab31db983fb7df267
Venki <18534venkanna@gmail.com>
2024-09-10 16:06:41 +0530

    Triggers wont haappen multiple times

--- WorkingTitle/Assets/EnvironmentTrigger.cs
+++ WorkingTitle/Assets/EnvironmentTrigger.cs
@@ -8,15 +8,24 @@ public class EnvironmentTrigger : MonoBehaviour
     public UnityEvent onTriggerEnter;
     public UnityEvent onTriggerExit;
     public UnityEvent onTriggerStay;
+    public bool destroyOnExit;
+    public bool destroyOnEnter;
 
     private void OnTriggerEnter(Collider other)
     {
+
         onTriggerEnter.Invoke();
+        
+        if(destroyOnEnter)
+           Destroy(gameObject);
     }
 
     private void OnTriggerExit(Collider other)
     {
         onTriggerExit.Invoke();
+
+       if(destroyOnExit)
+       Destroy(gameObject);
     }
 
     private void OnTriggerStay(Collider other)
